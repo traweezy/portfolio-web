bad060403be5072a41169525e43c45cfb1c3e12a
Tyler Schumacher <tyler.schumacher@protonmail.com>
2021-06-20 19:46:03 -0400

    💄 <style> Add eslint and prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,10 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = (): JSX.Element => (
+  <div className="p-6 items-center justify-center">
+    <h1 className="text-blue-400 font-medium">Hello World!</h1>
+    <p className="tracking-widest">This is my first React App.</p>
+  </div>
+);
 
 export default App;
