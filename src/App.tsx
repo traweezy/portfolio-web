5fd6cca5f215b9c6a5050bd889b099683a0ba9c8
Tyler Schumacher <tyler.schumacher@protonmail.com>
2021-06-21 19:07:37 -0400

    🔥 <chore> Remove unnecessary tests

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import Navigation from './components/navigaiton';
 
 const App = (): JSX.Element => (
   <div className="h-full grid grid-cols-12 grid-flow-col font-titillium">
-    <div className="border-r-2 border-black col-span-3">
+    <div className="border-r-2 border-black md:col-span-3 lg:col-span-1p">
       <Navigation />
     </div>
     <div className=" col-span-9" />
