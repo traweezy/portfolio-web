b990c60d5f8bacb87228245ea785699f72bc69ab
Tyler Schumacher <tyler.schumacher@protonmail.com>
2021-06-23 18:57:18 -0400

    👷 <chore> Add test action to CI

--- src/components/navigation/navigation.spec.tsx
+++ src/components/navigation/navigation.spec.tsx
@@ -1,5 +1,4 @@
 import { render, screen, cleanup } from '@testing-library/react';
-import userEvent from '@testing-library/user-event';
 // eslint-disable-next-line import/no-extraneous-dependencies
 import { createMemoryHistory } from 'history';
 import React from 'react';
