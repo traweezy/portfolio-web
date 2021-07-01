d647fb28fa37c125b6fa28c1e1cdc3ae3215695c
Tyler Schumacher <tyler.schumacher@protonmail.com>
2021-07-01 18:12:47 -0400

    ✨ <feat> Add page container component

--- src/app.tsx
+++ src/app.tsx
@@ -1,30 +1,10 @@
 import React from 'react';
 import { HashRouter as Router, Switch, Route } from 'react-router-dom';
-import ParticleBackground from 'react-particle-backgrounds';
 import Navigation from './components/navigation';
+import PageContainer from './components/page-container';
 import About from './pages/about/index';
 import Projects from './pages/projects';
 import Contact from './pages/contact';
-
-const settings: ParticleBackgroundConfig = {
-  particle: {
-    particleCount: 100,
-    color: '#ffb75f',
-    minSize: 2,
-    maxSize: 4,
-  },
-  velocity: {
-    directionAngle: 0,
-    directionAngleVariance: 30,
-    minSpeed: 0.2,
-    maxSpeed: 2,
-  },
-  opacity: {
-    minOpacity: 0,
-    maxOpacity: 0.5,
-    opacityTransitionTime: 5000,
-  },
-};
 
 const routes: RouteConfig[] = [
   {
@@ -57,11 +37,12 @@ const App = (): JSX.Element => (
         <Switch>
           {routes.map(({ name, path, exact, component: RouteComponent }) => (
             <Route key={name} path={path} exact={exact}>
-              <RouteComponent />
+              <PageContainer>
+                <RouteComponent />
+              </PageContainer>
             </Route>
           ))}
         </Switch>
-        <ParticleBackground settings={settings} />
       </div>
     </div>
   </Router>
---
+++ src/components/page-container/index.tsx
@@ -0,0 +1,35 @@
+import React, { ReactChild, ReactChildren } from 'react';
+import Particles from 'react-particle-backgrounds';
+
+const settings: ParticleBackgroundConfig = {
+  particle: {
+    particleCount: 100,
+    color: '#ffb75f',
+    minSize: 2,
+    maxSize: 4,
+  },
+  velocity: {
+    directionAngle: 0,
+    directionAngleVariance: 30,
+    minSpeed: 0.2,
+    maxSpeed: 2,
+  },
+  opacity: {
+    minOpacity: 0,
+    maxOpacity: 0.5,
+    opacityTransitionTime: 5000,
+  },
+};
+
+interface PageContainerProps {
+  children: ReactChild | ReactChildren;
+}
+
+const PageContainer = ({ children }: PageContainerProps) => (
+  <>
+    <Particles className="absolute z-0" settings={settings} />
+    <div className="relative z-10 h-full">{children}</div>
+  </>
+);
+
+export default PageContainer;
