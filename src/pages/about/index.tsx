fdeb5f7dff201a7230f400216d99d89f7435d32b
Tyler Schumacher <tyler.schumacher@protonmail.com>
2021-10-11 15:48:25 -0400

    ♻️ <refactor> Update about section with placeholder

--- src/pages/about/index.tsx
+++ src/pages/about/index.tsx
@@ -1,12 +1,3 @@
-const About = (): JSX.Element => (
-  <div className="text-lg">
-    I&apos;m a front-end web developer with a background in computer systems and
-    network infrastructure. My 8 years of IT experience has given me a strong
-    foundation for web development and building complex solutions. Recently, I
-    graduated from the Juno College Immersive Web Development Bootcamp. I am
-    passionate about coding and solving problems through code, and I am excited
-    to work alongside other amazing programmers and learn so much more!
-  </div>
-);
+const About = (): JSX.Element => <div className="text-lg">coming soon</div>;
 
 export default About;
