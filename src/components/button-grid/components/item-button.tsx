194d1357087bf61b75c20f1010f76b741c6ca818
Tyler Schumacher <tyler.schumacher@protonmail.com>
2021-10-12 12:02:14 -0400

    ♻️ <refactor> Update item button to be an anchor

--- src/components/button-grid/components/item-button.tsx
+++ src/components/button-grid/components/item-button.tsx
@@ -2,22 +2,18 @@ interface Props {
   item: ButtonGridItem;
 }
 
-const SkillButton = ({ item }: Props): JSX.Element => {
-  const handleClick = (link: string) => {
-    window.open(link, '_blank');
-  };
-
-  return (
-    <button
-      title={item.name}
-      className="flex flex-col items-center justify-around p-3 text-center bg-gray-100 border-2 border-gray-300 rounded-xl hover:bg-tangerine-500 transition duration-500 ease-in-out"
-      type="button"
-      onClick={() => handleClick(item.link)}
-    >
-      <img className="w-12 pb-2" src={item.image} alt={item.name} />
-      <span className="font-bold">{item.name}</span>
-    </button>
-  );
-};
+const SkillButton = ({ item }: Props): JSX.Element => (
+  <a
+    title={item.name}
+    className="flex flex-col items-center justify-around p-3 text-center bg-gray-100 border-2 border-gray-300 hover:shadow-xl rounded-xl hover:bg-tangerine-500 transition duration-500 ease-in-out"
+    type="button"
+    href={item.link}
+    target="_blank"
+    rel="noreferrer"
+  >
+    <img className="w-12 pb-2" src={item.image} alt={item.name} />
+    <span className="font-bold">{item.name}</span>
+  </a>
+);
 
 export default SkillButton;
