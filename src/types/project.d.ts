bfe3c8eaf9fbcbc3981941a086cce0117037a4ce
Tyler Schumacher <tyler.schumacher@protonmail.com>
2021-10-16 17:07:29 -0400

    🏷️ <chore> Add Project and Link types

---
+++ src/types/project.d.ts
@@ -0,0 +1,16 @@
+type LinkType = 'GITHUB' | 'SWAGGER' | 'PLAYGROUND' | 'LIVE';
+
+interface Link {
+  id: number;
+  url: string;
+  type: LinkType;
+}
+
+interface Project {
+  id: number;
+  name: string;
+  description: string;
+  image: string;
+  technologies: string[];
+  links: Link[];
+}
