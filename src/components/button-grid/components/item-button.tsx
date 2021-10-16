27131babece81bf4e5ddce3b2d1b0c7a584d7ffb
Tyler Schumacher <tyler.schumacher@protonmail.com>
2021-10-16 18:41:55 -0400

    💄 <style> Update styles for mobile view

--- src/components/button-grid/components/item-button.tsx
+++ src/components/button-grid/components/item-button.tsx
@@ -2,18 +2,20 @@ interface Props {
   item: ButtonGridItem;
 }
 
-const SkillButton = ({ item }: Props): JSX.Element => (
-  <a
-    title={item.name}
-    className="flex flex-col items-center justify-around max-w-sm p-3 py-4 text-center bg-gray-100 border-2 border-gray-300 md:py-0 md:hover:shadow-xl rounded-md md:rounded-xl md:hover:bg-tangerine-500 transition duration-500 ease-in-out"
-    type="button"
-    href={item.link}
-    target="_blank"
-    rel="noreferrer"
-  >
-    <img className="w-12 pb-2" src={item.image} alt={item.name} />
-    <span className="font-bold">{item.name}</span>
-  </a>
-);
+const SkillButton = ({ item }: Props): JSX.Element => {
+  const handleClick = (url: string): void =>
+    window.open(url, '_blank')?.focus();
+
+  return (
+    <button
+      type="button"
+      className="flex flex-col items-center justify-around max-w-sm p-3 text-center bg-gray-100 border-2 border-gray-300 md:hover:shadow-xl rounded-md md:rounded-xl md:hover:bg-tangerine-500 transition duration-500 ease-in-out"
+      onClick={() => handleClick(item.link)}
+    >
+      <img className="w-12 pb-2" src={item.image} alt={item.name} />
+      <span className="font-bold">{item.name}</span>
+    </button>
+  );
+};
 
 export default SkillButton;
--- src/components/button-grid/index.tsx
+++ src/components/button-grid/index.tsx
@@ -4,16 +4,14 @@ interface Props {
   items: ButtonGridItem[];
 }
 
-const ButtonGrid = ({ items }: Props): JSX.Element => {
-  return (
-    <div className="container mx-auto">
-      <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-6 gap-6">
-        {items.map(item => (
-          <SkillButton key={item.name} item={item} />
-        ))}
-      </div>
+const ButtonGrid = ({ items }: Props): JSX.Element => (
+  <div className="container mx-auto">
+    <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-6 gap-6">
+      {items.map(item => (
+        <SkillButton key={item.name} item={item} />
+      ))}
     </div>
-  );
-};
+  </div>
+);
 
 export default ButtonGrid;
