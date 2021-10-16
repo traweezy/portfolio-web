a3f6676fc3378ecba91efe9d66d508ceb00a333b
Tyler Schumacher <tyler.schumacher@protonmail.com>
2021-10-16 17:06:40 -0400

    ✨ <feat> Add spinner component

---
+++ src/components/spinner/index.tsx
@@ -0,0 +1,7 @@
+const Spinner = () => (
+  <div className="flex items-center justify-center">
+    <div className="w-32 h-32 border-b-2 rounded-full border-tangerine-900 animate-spin" />
+  </div>
+);
+
+export default Spinner;
