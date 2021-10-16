6f88a151ed142b9f368dcf8120415f65c7c74882
Tyler Schumacher <tyler.schumacher@protonmail.com>
2021-10-16 17:46:19 -0400

    🐛 <bug> Fix mobile project button stlye

--- src/pages/contact/index.tsx
+++ src/pages/contact/index.tsx
@@ -161,7 +161,7 @@ const Contact = (): JSX.Element => {
             <div className="w-4 h-4 border-b-2 border-gray-900 rounded-full animate-spin" />
           </div>
         ) : (
-          'Send'
+          'SEND'
         )}
       </button>
     </form>
